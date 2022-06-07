a1b50a9e4165952f9bcc91f3beeae66de217d028
MhamadHFarhan <moh.hesham.f@gmail.com>
2022-06-07 12:33:22 +0300

    Create the server and structure files

---
+++ backend/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+require("dotenv").config();
+
+const app = express();
+
+// Mongoose Connection
+try {
+  mongoose.connect("mongodb://localhost/test");
+  console.log("Connected to MongoDB");
+} catch (error) {
+  throw error;
+}
+//
+
+//  Middleware
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(bodyParser.json({ limit: "2mb" }));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log("Server started on port 5000");
+});
