81743b53cfcbbdc1ce7f323acf965e03bf4fa6a8
MhamadHFarhan <moh.hesham.f@gmail.com>
2022-06-14 12:38:43 +0300

    Create add .like comment + like and reply to +

--- backend/controllers/comment.js
+++ backend/controllers/comment.js
@@ -18,4 +18,56 @@ const createNewComment = async (req, res) => {
   }
 };
 
-module.exports = { createNewComment };
+const addLike = async (req, res) => {
+  const { commentId } = req.params;
+  const userId = req.user._id;
+  try {
+    const comment = await commentModel.findByIdAndUpdate(
+      { _id: commentId },
+      {
+        $addToSet: { likes: userId },
+      },
+      { new: true }
+    );
+    res.json({ comment });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const removeLike = async (req, res) => {
+  const { commentId } = req.params;
+  const userId = req.user._id;
+  try {
+    const comment = await commentModel.findByIdAndUpdate(
+      { _id: commentId },
+      {
+        $pull: { likes: userId },
+      },
+      { new: true }
+    );
+    res.json({ comment });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const replyTo = async (req, res) => {
+  const { commentId } = req.params;
+  const { text } = req.body;
+  const user = req.user._id;
+  try {
+    const comment = await commentModel.findByIdAndUpdate(
+      { _id: commentId },
+      {
+        $addToSet: { replies: { text, user } },
+      },
+      { new: true }
+    );
+    res.json({ comment });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = { createNewComment, addLike, removeLike, replyTo };
--- backend/routes/comment.js
+++ backend/routes/comment.js
@@ -1,9 +1,17 @@
 const express = require("express");
-const { createNewComment } = require("../controllers/comment");
+const {
+  createNewComment,
+  addLike,
+  removeLike,
+  replyTo,
+} = require("../controllers/comment");
 const authCheck = require("../middleware/authCheck");
 
 const commentRouter = express.Router();
 
 commentRouter.post("/", authCheck, createNewComment);
+commentRouter.post("/:commentId/like", authCheck, addLike);
+commentRouter.post("/:commentId/unlike", authCheck, removeLike);
+commentRouter.post("/:commentId/reply", authCheck, replyTo);
 
 module.exports = commentRouter;
